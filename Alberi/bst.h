79d181797bf36a4b2b41b14a4cc9778808293e32
Universita <gaby.riscica@gmail.com>
2022-07-22 18:22:47 +0200

    BST

--- Alberi/bst.h
+++ Alberi/bst.h
@@ -3,69 +3,68 @@
 #include "node.h"
 
 template <typename T>
-class bst{
+class BST{
     private: 
-        Node<T>* root;
-    public:
-        bst(){
+    Node<T>* root;
+
+    public: 
+    BST(){
         root = nullptr;
     }
 
-   // controllo se è vuota
+    //controllo se è vuota
     bool isEmpty(){
         return root == nullptr;
     }
 
-    #pragma region// inserisco un nodo
-    void insert (T key){
-        if (this->isEmpty()){
-            root = new Node<T>(key);
+    #pragma region
+    void insert(T val){
+        if(this->isEmpty()){
+            root = new Node<T>(val);
             return;
         }
-        insert (root, key);
+
+        insert(root, val);
     }
 
-    void insert (Node<T>* ptr, T key){
-
-        if(ptr == nullptr){
-            ptr = new Node<T>(key);
+    void insert (Node<T>* ptr, T val){
+        if (ptr == nullptr){
+            ptr = new Node<T>(val);
         }
 
-        if(ptr->left == nullptr && key<=ptr->val){
-            ptr->left = new Node<T>(key);
-            ptr->left->setParent(ptr); //ricordati di settare il parent
+        if (ptr->left == nullptr && val <= ptr->val){
+            ptr->left = new Node<T>(val);
+            ptr->left->setParent(ptr);  //bisogna settare il parent
             return;
         }
 
-        else if(ptr->right == nullptr && key > ptr->val){
-            ptr->right = new Node<T>(key);
-            ptr->right->setParent(ptr); //ricordati di settare il parent
+        else if (ptr->right == nullptr && val > ptr->val){
+            ptr->right = new Node<T>(val);
+            ptr->right->setParent(ptr); //bisogna settare il parent
             return;
         }
 
-        else if (key <= ptr->val){
-            ptr = ptr->left;
-            insert(ptr, key);
+        else if(val <= ptr->val){
+            insert(ptr->left, val);
         }
 
         else{
-            ptr = ptr->right;
-            insert(ptr, key);
+            insert(ptr->right, val);
         }
     }
+
     #pragma endregion
-    //stampo un nodo
-    #pragma region //visita
+
+    #pragma region "Visit"
     void visit(Node<T>* ptr){
         cout << *ptr;
     }
 
-    // visita inorder, preorder, postorder
     void inorder(Node<T>* ptr){
-        if (ptr == nullptr){
+        if(ptr == nullptr){
             return;
         }
-
+        
         inorder(ptr->left);
         visit(ptr);
         inorder(ptr->right);
@@ -76,10 +75,10 @@ class bst{
     }
 
     void preorder(Node<T>* ptr){
-        if (ptr == nullptr){
+        if(ptr == nullptr){
             return;
         }
-
+        
         visit(ptr);
         preorder(ptr->left);
         preorder(ptr->right);
@@ -88,12 +87,13 @@ class bst{
     void preorder(){
         preorder(root);
     }
-    
+
     void postorder(Node<T>* ptr){
-        if (ptr == nullptr){
+        if(ptr == nullptr){
+            
             return;
         }
-
+        
         postorder(ptr->left);
         postorder(ptr->right);
         visit(ptr);
@@ -102,163 +102,163 @@ class bst{
     void postorder(){
         postorder(root);
     }
-    #pragma endregion
     
-    #pragma region// ricerche varie
-    Node<T>* min(){
-            return min(root);
-        }
+    #pragma endregion
 
-    Node<T>* min(Node<T>* start){
+    #pragma region "Ricerche"
+    Node<T>* min(){
+        return min(root);
+    }
+
+    Node<T>* min(Node<T>* ptr){
         if(isEmpty()){
             return nullptr;
         }
-        //Node<T>* ptr = start; //il puntatore va inizializzato qua non va passato come parametro, non va inizializzato a root.
-        while (start->left){
-            start = start->left;    //NON è ricorsiva, MA è ITERATIVA.
+        while(ptr->left){
+            ptr = ptr->left;
         }
 
-        return start;
+        return ptr;
     }
-   
-   // ricerca del maggiore
-   Node<T>* max(){
-            return max(root);
-        }
-   
-   Node<T>* max (Node<T>* start){
-    if(isEmpty()){
+
+    Node<T>* max(){
+        return max(root);
+    }
+
+    Node<T>* max(Node<T>* ptr){
+        if(isEmpty()){
+            cout << "empty bst" << endl;
             return nullptr;
         }
-           //il puntatore va inizializzato qua non va passato come parametro
-        while (start->right){
-            start = start->right;
+        while(ptr->right){
+            ptr = ptr->right;
         }
 
-        return start;
-   }
+        return ptr;
+    }
 
-    // ricerca del successore
-    Node<T>* succ(Node<T>* x){
-        if(this->isEmpty()){
+    Node<T>* suc(Node<T>* x){
+        if(x == nullptr){
             return nullptr;
         }
 
-        if (x->right){
+        if(x->right){
             return min(x->right);
         }
 
-        Node<T>* y = x->parent;       // va inizializzato al puntatore del genitore
-        while(y!= nullptr && x == y->right){ //controllare anche che non "sia finito"
+        Node<T>* y = x->parent;
+        while(y!=nullptr && x == y->right){
             x = y;
             y = x->parent;
         }
 
-        return y; 
+        return y;
     }
 
-    // ricerca del predecessore
-    Node<T>* prec(Node<T>* ptr){
-        if(isEmpty()){
-            return nullptr;
+    Node<T>* prec(Node<T>* x){
+        if(x == nullptr){
+           return nullptr;
         }
 
-        if (ptr->left){
-            return max(ptr->left);
+        if(x->left){
+            x = max(x->left);
+            return x;
         }
 
-        Node<T>* x = ptr->parent;
-        while(x!= nullptr && ptr == x->left){ //controllare anche che non "sia finito"
-            ptr = x;
-            x = ptr->parent;
+        Node<T>* y = x->parent;
+        while(y!=nullptr && x == y->left){
+            x = y;
+            y = x->parent;
         }
 
-        return x; 
+        return y;
     }
 
-    // ricerca
-    Node<T>* search(T key){
-        return search(key, root);  //eseternamente dico "ok cercami questo, internamente dico ok ti do il puntatore"
+    Node<T>* search(T val){
+        return search(val, root);
     }
-
-    Node<T>* search(T key, Node<T>* ptr){
-        if (ptr == nullptr){
-            return nullptr;
-        }
-
-        if (ptr->val == key){
+    Node<T>* search (T val, Node<T>* ptr){
+      if(ptr == nullptr){
+        return nullptr;
+      }
+      if(val == ptr->val){
             return ptr;
         }
 
-        if(key<ptr->val){
-            return search(key, ptr->left);
+        if (val< ptr->val){
+            return search(val, ptr->left);
         }
 
-        if(key>ptr->val){
-            return search(key, ptr->right);
+        if(val > ptr->val){
+            return search(val, ptr->right);
         }
+
         return nullptr;
     }
-
+    
     #pragma endregion
-    //eliminazione 1 o 2 caso
-    #pragma region //eliminazione 
-    Node<T>* removefs(Node<T>* ptr){
-        if(ptr->left == nullptr && ptr->right == nullptr){
+
+    #pragma region "Rimozioni"
+
+    Node<T>* remove(Node<T>* ptr){
+        if (ptr->left == nullptr && ptr->right == nullptr){
             if(ptr == ptr->parent->left){
                 ptr->parent->left = nullptr;
             }
             else{
-                ptr->parent->right=nullptr;
+                ptr->parent->right = nullptr;
             }
-            return ptr;  // tornare il puntatore
+            return ptr;
         }
 
-        if(ptr->left == nullptr && ptr->right != nullptr){
+        if(ptr->left == nullptr && ptr->right!=nullptr){
             ptr->right->parent = ptr->parent;
-            if(ptr->parent->right == ptr){
+            if(ptr == ptr->parent->left){
+                ptr->parent->left = ptr->right;
+            }
+            else{
                 ptr->parent->right = ptr->right;
             }
-            else{
-                ptr ->parent->left = ptr->right;
-            }
-            return ptr; // tornare il puntatore
+            return ptr;
         }
 
-        if(ptr->right == nullptr && ptr->left != nullptr){
+        if(ptr->left != nullptr && ptr->right==nullptr){
             ptr->left->parent = ptr->parent;
-            if(ptr->parent->right == ptr){
-                ptr->parent->right = ptr->left;
-            }
-            else{
+            if(ptr == ptr->parent->left){
                 ptr->parent->left = ptr->left;
             }
-            return ptr; // tornare il puntatore
+            else{
+                ptr->parent->right = ptr->left;
+            }
+            return ptr;
         }
 
         return nullptr;
-
     }
 
-    //eliminazione "completa"
-    Node<T>* remove(T key){
-        if (isEmpty())
+    Node<T>* remove(T val){
+        if(isEmpty()){
+            cout << "empty bst" << endl;
             return nullptr;
-
-        Node<T>* node = this->search(key);        // prima di eliminare un nodo dobbiamo essere sicuri che esiste 
-        if (node == nullptr){
-        return nullptr;
         }
 
-        Node<T>* temp = removefs(node);                   // il valore passato è il puntatore TROVATO CERCANDO IL VALORE DA ELIMINARE
-        if(temp != nullptr)             // torniamo temp per assicurarci che sia stata effettuate l'eliminazione
-            return temp;
+        Node<T>* node = this->search(val);
+        if (node == nullptr){
+            cout << "value not found" << endl;
+            return nullptr;
+        }
 
-        Node<T>* suc = succ(node);
-        T swap = suc->val;                  // facciamo lo swap direttamente qua
-        suc->val = node ->val;
-        node->val = swap;
-        temp = removefs(suc);
+        Node<T>* temp = remove(node);
+        if (temp!=nullptr){
+            return temp;
+        }
+
+        Node<T>* sux = suc(node);
+        T swap = node->val;
+        node->val = sux->val;
+        sux->val = val;
+
+        temp = remove(sux);
         return temp;
     }
     #pragma endregion
--- Alberi/node.h
+++ Alberi/node.h
@@ -1,39 +1,46 @@
 #ifndef BST_NODE_H
 #define BST_NODE_H
-
-#include <iostream> 
-
+#include<iostream>
 using namespace std;
 
 template <typename T>
 class Node{
     private:
-    
     T val;
-    Node<T>* left;
-    Node<T>* right;
     Node<T>* parent;
+    Node<T>* left; 
+    Node<T>* right;
     
     template <typename U>
-    friend class bst;
+    friend class BST;
 
     public: 
-    Node(T key) : val(key){
+    Node(T val) : val(val){
         left = nullptr;
         right = nullptr;
-        parent = nullptr;   // va inizializzato anche il genitore
+        parent = nullptr;
     }
 
-   void setParent(Node<T>* parent){
-        this->parent = parent;
+    #pragma region "Setter"
+    void setKey(T key){
+        this->val = key;
     }
 
-    void setLeft(Node<T>* left){
-        this->left = left;
+    void setParent(Node<T>* ptr){
+        this->parent = ptr;
     }
 
-    void setRight(Node<T>* right){
-        this->right = right;
+    void setLeft(Node<T>* ptr){
+        this->left = ptr;
+    }
+
+    void setRight(Node<T>* ptr){
+        this->right = ptr;
+    }
+    #pragma endregion
+    #pragma region "Getter"
+    T getKey(){
+        return this->val;
     }
 
     Node<T>* getParent(){
@@ -47,19 +54,13 @@ class Node{
     Node<T>* getRight(){
         return this->right;
     }
-
-    void setKey(T key){
-        this->val = key;
-    }
-
-    T getVal(){
-        return this->val;
-    }
     
-    friend ostream& operator<<(ostream& out, Node<T> &node){
-        out << endl << "node val: " << node.val << " " << &node << " - left: " << node.left << " - right: " << node.right << " - parent: " << node.parent;
-        return out;
-    }    
-};
+    #pragma endregion
 
+    //overload di stampa
+    friend ostream& operator<< (ostream& out, Node<T> &node){
+        out << "node val: " << node.val << " " << &node << " - left: " << node.left << " - right: " << node.right << " - parent: " << node.parent << endl;
+        return out;
+     }
+};
 #endif
