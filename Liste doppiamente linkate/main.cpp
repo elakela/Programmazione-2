f90b3af91f4a4d43a03c2fe56121c06aba0d1e6a
Universita <gaby.riscica@gmail.com>
2022-07-22 18:25:35 +0200

    liste doppiamente linkate

--- Liste doppiamente linkate/main.cpp
+++ Liste doppiamente linkate/main.cpp
@@ -1,50 +1,34 @@
-#include "dllist.h"
+#include "list.h"
 
 int main(){
-    DLList<int> lista;
-
-    lista.insertHead(10);
-    lista.insertHead(15);
-    lista.insertHead(23);
-    lista.insertHead(28);
-
+    List<int> lista;
+    cout << lista.isEmpty() << endl;
+    lista.insertHead(13);
+    lista.insertHead(16);
+    lista.insertHead(25);
     cout << lista;
-    system("pause");
+    system ("pause");
     system("cls");
 
-    lista.insertTail(19);
-    lista.insertTail(2);
+    lista.insertTail(23);
+    lista.insertTail(12);
     cout << lista;
-    system("pause");
+    system ("pause");
+    system("cls");
+
+    lista.insertCrescente(1); 
+    lista.insertDec(50);
+    lista.insertDec(1);
+    lista.insertCrescente(51);
+     cout << lista;
+    system ("pause");
     system("cls");
 
     lista.removeHead();
-    lista.removeHead();
-    lista.removeHead();
-    cout << lista << endl;
-    system("pause");
-    system("cls");
-
     lista.removeTail();
-    cout << lista << endl;
-    system("pause");
+    lista.removeVal(25);
+    cout << lista;
+    system ("pause");
     system("cls");
 
-
-    cout << endl;
-    lista.removeKey(19);
-    cout << lista;
-    lista.removeKey(13);
-    system("pause");
-    system("cls");
-    cout << lista;
-
-    lista.insertOrder(8);
-    lista.insertOrder(23);
-    lista.insertOrder(20);
-    lista.insertOrder(15);
-    system("pause");
-    system("cls");
-    cout << lista;
-    
 }
